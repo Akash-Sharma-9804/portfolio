96a5580b6c4d922c367b77a295952b430a9333ee
Akash Sharma <akashsharma9804@gmail.com>
2025-03-13 00:44:38 +0530

    resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,12 +17,12 @@ const iconvariation= (duration)=>({
 });
 const cvDownload = () => {
   // Path to my CV file
-  const cvUrl = './cv/Resume of Akash.pdf';
+  const cvUrl = './cv/Resume of Akash Sharma.pdf';
 
 
   const link = document.createElement('a');
   link.href = cvUrl;
-  link.download = 'Resume of Akash.pdf'; 
+  link.download = 'Resume of Akash Sharma.pdf'; 
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
